118c2524e641b647c61c9cd2a94b7a9d553354af
Daniel Perez <danielpm926@gmail.com>
2019-08-05 19:57:42 -0500

    File is ready for MongoDB

--- server/api/v1/tasks/controller.js
+++ server/api/v1/tasks/controller.js
@@ -3,21 +3,6 @@ const uuidv4 = require("uuid/v4");
 
 exports.create = (req, res, next) => {
   const { body = {} } = req;
-  const title = body.title;
-  const description = body.description;
-  const uu_id = uuidv4();
-  const creationDate = new Date();
-  const task = {
-    title,
-    description,
-    id: uu_id,
-    creationDate,
-  };
-  tasks.push(task.title);
-  tasks.push(task.description);
-  tasks.push(task.id);
-  tasks.push(task.creationDate);
-  res.json(tasks);
 };
 
 exports.all = (req, res, next) => {
@@ -27,11 +12,6 @@ exports.all = (req, res, next) => {
 exports.read = (req, res, next) => {
   const { params = {} } = req;
   const { id = 1 } = params;
-  for (let i = 0; i < tasks.length; i++) {
-    if (id == tasks[i].id) {
-      console.log(tasks[i].title);
-    }
-  }
   res.json({
     id,
   });
