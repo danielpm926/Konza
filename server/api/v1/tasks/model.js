82300d4f180cd9c403855d0eaf9edc1cceae3883
Daniel Perez <danielpm926@gmail.com>
2019-08-09 20:28:25 -0500

    Added the Sorting functionality

--- server/api/v1/tasks/model.js
+++ server/api/v1/tasks/model.js
@@ -2,6 +2,15 @@ const mongoose = require("mongoose");
 
 const { Schema } = mongoose;
 
+/*
+ * El Schema de mongoose nos brinda muchas
+ * funcionalidades como establecer una validacion
+ * basica para el modelo, asi como funciones de
+ * quitar los espacios de sobra en los valores de
+ * los campos y demas, inclusive colocar un valor
+ * por defecto.
+ */
+
 const fields = {
   title: {
     type: String,
@@ -25,8 +34,19 @@ const fields = {
   },
 };
 
+/*
+ * El Schema tambien permite establecer ciertas
+ * opciones una de ellas son los timestamps para
+ * que se creen los campos createdAt y updatedAt
+ * de tipo fecha cada vez que se cree o actualice
+ * un registro respectivamente
+ */
+
 const task = new Schema(fields, {
   timestamps: true,
 });
 
-module.exports = mongoose.model("task", task);
+module.exports = {
+  Model: mongoose.model("task", task),
+  fields,
+};
